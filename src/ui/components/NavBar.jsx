8a05719a32a24794b0b380888d0741d326ff3863
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-26 18:11:54 -0300

    Fix: add navigate to login page

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,6 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+  const navigate = useNavigate(); // custom hooks
+
+  const handleLogout = () => {
+    navigate('/login', {
+      replace: true
+    })
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -31,11 +39,11 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">
-            Oscar
-          </span>
-          
-          <button className="nav-item nav-link btn">Logout</button>
+          <span className="nav-item nav-link text-primary">Oscar</span>
+
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
+            Logout
+          </button>
         </ul>
       </div>
     </nav>
