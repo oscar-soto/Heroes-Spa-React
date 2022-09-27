2bd30e7ddd352668d9d34ec0f215f4fdbb2120c1
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-27 00:51:05 -0300

    Fix: add Hero Card componeents

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,13 +1,17 @@
+import { HeroCard } from './';
 import { getHeroesByPublisher } from '../helpers';
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
-      {heroes.map(({ id, superhero }) => (
-        <li key={id}>{superhero}</li>
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
+      {heroes.map((hero) => (
+        <HeroCard 
+          key={hero.id} 
+          {...hero} 
+        />
       ))}
-    </ul>
+    </div>
   );
 };
