5bb19595f7d14a8db534517bda9dfc1ce6167bc4
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-27 11:57:44 -0300

    Fix: read url parameters

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,17 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { getHerobyid } from '../helpers';
+
 export const HeroPage = () => {
+  const { id } = useParams();
+  const hero = getHerobyid(id);
+
+  if (!hero) {
+    return <Navigate to='/marvel' />
+  }
+
   return (
-    <h1>HeroPage</h1>
-  )
-}
+    <>
+      <h1>{hero.superhero}</h1>
+    </>
+  );
+};
