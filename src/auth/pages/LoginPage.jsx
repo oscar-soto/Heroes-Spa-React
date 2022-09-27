0ca37238c7d9cb1ffc99dccdb6e0041cacec7d07
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-26 22:23:57 -0300

    Fix: navigate in login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,22 @@
+import { useNavigate } from 'react-router-dom';
+
 export const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const hadlenLogin = () => {
+    navigate('/', {
+      replace: true,
+    });
+  };
+
   return (
-    <h1>LoginPage</h1>
-  )
-}
+    <div className="container mt-5">
+      <h1>Login</h1>
+      <hr />
+
+      <button className="btn btn-primary" onClick={hadlenLogin}>
+        Login
+      </button>
+    </div>
+  );
+};
