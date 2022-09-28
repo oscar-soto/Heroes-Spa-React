c64991e08e01b8af664c2e87a266b98a3dcf8645
Oscar <oscarantonio.soto1998@gmail.com>
2022-09-28 00:51:24 -0300

    Fix: add query to search

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,27 @@
-import { HeroCard } from "../components";
+import { useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string';
+
+import { useForm } from '../../hooks/userForm';
+import { HeroCard } from '../components';
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const {q = ''} = queryString.parse(location.search);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: '',
+  });
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    if (searchText.trim().length <= 1) return;
+
+    const params = searchText.toLowerCase().trim();
+    navigate(`?q=${params}`);
+  };
+
   return (
     <>
       <h1>Search</h1>
@@ -11,13 +32,15 @@ export const SearchPage = () => {
           <h4>searching</h4>
           <hr />
 
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <input
               type="text"
               placeholder="Search a hero"
               className="form-control"
               name="searchText"
               autoComplete="off"
+              value={searchText}
+              onChange={onInputChange}
             />
 
             <button className="btn btn-outline-primary mt-1">Search</button>
@@ -28,12 +51,10 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-success">
-            Search a hero
-          </div>
+          <div className="alert alert-success">Search a hero</div>
 
           <div className="alert alert-danger">
-            No hero with <strong>ABC</strong>
+            No hero with <strong>{q}</strong>
           </div>
 
           {/* <HeroCard /> */}
