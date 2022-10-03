f7d05c477d2cf35d6be26875133a0b5d180d3742
Oscar <oscarantonio.soto1998@gmail.com>
2022-10-03 00:27:18 -0300

    Fix: public and private routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,15 +2,31 @@ import { Route, Routes } from 'react-router-dom';
 
 import { LoginPage } from '../auth';
 import { HeoresRoutes } from '../heroes';
-import { Navbar } from '../ui';
+import { PrivateRoute, PublicRoute } from './';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route path="login" element={
+          <PublicRoute>
+            {/* <LoginPage /> */}
+            <Routes>
+              <Route path="/*" element={<LoginPage />} />
+            </Routes>
+          </PublicRoute>
+        } />
 
-        <Route path="/*" element={<HeoresRoutes />} />
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeoresRoutes />
+            </PrivateRoute>
+          }
+        />
+        {/* <Route path="login" element={<LoginPage />} /> */}
+        {/* <Route path="/*" element={<HeoresRoutes />} /> */}
       </Routes>
     </>
   );
