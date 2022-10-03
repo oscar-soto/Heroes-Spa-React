11d013b6f00faadd61cb8b9d31630d864b3c0264
Oscar <oscarantonio.soto1998@gmail.com>
2022-10-03 14:54:47 -0300

    Fix: testing to types.js

---
+++ __test__/auth/types/types.test.js
@@ -0,0 +1,10 @@
+import { types } from '../../../src/auth';
+
+describe('Pruebas en "Types.js"', () => {
+  test('Debe de regresar estos types', () => {
+    expect(types).toEqual({
+      login: '[Auth] Login',
+      logout: '[Auth] Logout',
+    });
+  });
+});
