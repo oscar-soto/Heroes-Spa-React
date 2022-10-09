72efd69083a79c0f770c24aa72038a9052cbb903
Oscar <oscarantonio.soto1998@gmail.com>
2022-10-09 13:18:32 -0300

    Fix: testing to AppRouter

---
+++ __test__/router/AppRouter.test.jsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthContext } from '../../src/auth';
+import { AppRouter } from '../../src/router';
+
+describe('Preubas en <AppRouter />', () => {
+  test('Debe de mostrar el login si no esta autenticado', () => {
+    const contextValue = {
+      logged: false,
+    };
+
+    render(
+      <MemoryRouter initialEntries={['/marvel']}>
+        <AuthContext.Provider value={contextValue}>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getAllByText('Login').length).toBe(2);
+  });
+
+  test('Debe de mostrar el componente de Marvel si esta autenticado', () => {  
+    const contextValue = {
+      logged: true,
+      user: {
+        id: 'ABC',
+        name: 'Oscar Espinoza'
+      }
+    };
+
+    render(
+      <MemoryRouter initialEntries={['/login']}>
+        <AuthContext.Provider value={contextValue}>
+          <AppRouter />
+        </AuthContext.Provider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getAllByText('Marvel').length).toBeGreaterThanOrEqual(1)
+  })
+});
